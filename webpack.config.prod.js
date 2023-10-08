07364e2832e4ed59044bfa2d1bf6711cfa746750
Kornel Janczak <korneljanczak10@gmail.com>
2023-10-08 14:33:57 +0200

    a

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanPlugin = require("clean-webpack-plugin");
+// const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
